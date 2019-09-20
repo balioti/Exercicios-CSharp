28b7fcf1039f66893ce546cb68bc3c16679ce0ff
Everton Balioti <balioti1@hotmail.com>
2019-09-20 11:16:16 -0300

    tabuada do 5

---
+++ Tabuada do 5 e for/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Tabuada_do_5_e_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("tabuada 5");
+            Console.WriteLine("informe um número");
+            int num = int.Parse(Console.ReadLine());
+
+			for (int num = 1; x <= 10; x++)
+			{
+				Console.WriteLine(num + " * " + num + " = " + num * x);
+			}
+        }
+    }
+}
