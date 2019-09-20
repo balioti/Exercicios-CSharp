505137a8a25f274b02ceef3a9669520738836366
Everton Balioti <balioti1@hotmail.com>
2019-09-20 11:06:21 -0300

    numero menor que 100

---
+++ 100primeirosnum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _100primeirosnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("numero menor que 100");
+            for(int num=1; num <100; num = num + 2){
+                Console.Write("-" + num);
+
+            }
+
+            
+        
+            
+        }
+    }
+}
--- parimpar/Program.cs
+++ parimpar/Program.cs
@@ -6,15 +6,28 @@ namespace parimpar
     {
         static void Main(string[] args)
         {
+            int num;
+            string op = "s";
+            while (op == "s")
+            {
+
             Console.WriteLine("informe se é par ou ímpar: ");
-            int num = int.Parse(Console.ReadLine());
+            num = int.Parse(Console.ReadLine());
 
             if(num % 2 == 0)
             {
-                Console.WriteLine("o número é PAIR");
+                Console.WriteLine("o número é PAR");
             }
             else {
-                Console.WriteLine("o número é ODD");
+                Console.WriteLine("o número é ÍMPAR");
+            }
+
+            Console.WriteLine("desejar inserir outro número? s/n");
+            op = Console.ReadLine();
+            
+
+
+            
             }
         }
     }
