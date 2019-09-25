d9af4bea214f8586acb7b52c7e46723c3d7e7a4e
Everton Balioti <balioti1@hotmail.com>
2019-09-25 11:17:27 -0300

    Pograma falho Vetores

---
+++ Triangulo A/triangulo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace triangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ vetores/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace vetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] new int[6], par = 0, impar = 0, i = 0;
+            for(int i = 1; i < 6; i++ )
+
+            Console.WriteLine("insira o " + i + "numero");
+            
+        }
+    }
+}
