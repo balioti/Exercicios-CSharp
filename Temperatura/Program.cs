d5e2f678b7cd17962e2251b66804dfbdef691f38
Everton Balioti <balioti1@hotmail.com>
2019-09-26 11:17:03 -0300

    Temperatura aula 26/09

---
+++ Temperatura/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Temperatura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] temperaturas = new double[12];
+            double maior = 0;
+            double menor = 0;
+
+            for(int i = 0; i < 12; i++){
+                Console.Write($"digite a temperatura do mes {i+1};");
+                temperaturas[i] = double.Parse(Console.ReadLine());
+            }
+
+            maior = temperaturas[0];
+            menor = temperaturas[0];
+            
+            foreach (var temp in temperaturas)
+            {
+                if(temp > maior){
+                    maior = temp;
+                } else if(temp < menor){
+                    menor = temp;
+                }
+            }
+
+            Console.WriteLine($"A maior temperatura é {maior}");
+            Console.WriteLine($"A menor temperatura é {menor}");
+        }
+    }
+}
--- vetores/Program.cs
+++ vetores/Program.cs
@@ -6,11 +6,28 @@ namespace vetores
     {
         static void Main(string[] args)
         {
-            int[] new int[6], par = 0, impar = 0, i = 0;
-            for(int i = 1; i < 6; i++ )
+            int[] vetor = new int[6];
+            int pares = 0;
+            int impares = 0;
 
-            Console.WriteLine("insira o " + i + "numero");
-            
+            for(int cont = 0; cont < 6; cont++){
+                Console.Write("digite um numero: ");
+                vetor[cont] = int.Parse(Console.ReadLine());
+            }
+
+            foreach (int num in vetor)
+            {
+                if(num%2 == 0){
+                    pares +=1;
+                    
+                } else {
+                    impares++;
+                }
+
+
+            }
+
+            Console.WriteLine($"vc tem {pares} numeros pares e {impares} numeros impares");
         }
     }
 }
