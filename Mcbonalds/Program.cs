66767fe542837a56d21e55db063d6cd924172de2
Everton Balioti <balioti1@hotmail.com>
2019-09-27 11:17:02 -0300

    Atributos

---
+++ Mcbonalds/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Mcbonalds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            cliente cliente1 = new cliente("Alexandre", "55 11 1223-4567", "alexandre@hotmail.com");
+
+            Console.WriteLine("Nome: " + cliente1.Nome);
+            Console.WriteLine("Telefone" + cliente1.Telefone);
+            Console.WriteLine("Email: " + cliente1.Email);
+        }
+    }
+}
---
+++ Mcbonalds/cliente.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Mcbonalds
+{
+    public class cliente
+    {
+        //Atributos
+        public string Nome {get;set;}
+        public string Endereço {get;set;}
+        public string Telefone {get;set;}
+        public string Senha {get;set;}
+        public string Email {get;set;}
+        DateTime DataNascimento {get;set;}
+
+        //Construtor
+
+        public cliente(string nome, string telefone, string email){
+            this.Nome = nome;
+            this.Telefone = telefone;
+            this.Email = email;
+
+        }
+
+
+    }
+}
