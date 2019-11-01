ff53577f49450f9e472f9e545ebe582d53c7abde
Everton Balioti <balioti1@hotmail.com>
2019-11-01 11:09:28 -0300

    comit zoo

---
+++ Zoo logico/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Zoo_logico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("[1] - Zebra");
+            System.Console.WriteLine("[2] - Tubarão Martelo");
+            System.Console.WriteLine("[3] - Gavião");
+            System.Console.WriteLine("[4] - Condor");
+            System.Console.WriteLine("[5] - Ibex");
+            System.Console.WriteLine("[6] - Mico Leão Dourado");
+            System.Console.WriteLine("[7] - Gorila");
+            System.Console.WriteLine("[8] - Elefante");
+            System.Console.WriteLine("[9] - Rã");
+            System.Console.WriteLine("[10] - Camaleão");
+            System.Console.WriteLine("[11] - Camelo");
+            System.Console.WriteLine("[12] - Píton");
+        }
+    }
+}
